b4522681cf71408b86dd48b23da34c242a577aa0
= <apadavid@gmail.com>
2023-04-19 18:32:14 +0200

    [ADD] constants file added

---
+++ app/src/main/java/com/example/hogwartsdata/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.hogwartsdata.utils
+
+object Constants {
+    const val BASE_END_POINT_URL = "https://wizard-world-api.herokuapp.com"
+    const val HOUSES_END_POINT = "/houses"
+}
